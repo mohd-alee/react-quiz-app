890b59c1d4c137c29dcd56ec2fdda08d6f328687
mohd-alee <syed.ali_11@hotmail.com>
2022-11-24 03:02:13 +0500

    Grading issue

--- src/App.js
+++ src/App.js
@@ -7,12 +7,29 @@ function App() {
   const [questionIndex, setQuestionIndex] = useState(0);
   const [userAnswer, setUserAnswer] = useState(0);
   const [correctAnswers, setCorrectAnswers] = useState(0);
+  const [scorePercentage, setScorePercentage] = useState(0);
+  const [scoreGrade, setScoreGrade] = useState(0);
+
   const submitAnswer = ()=>{
     if(quizData[questionIndex].correctAnswerIndex === userAnswer){
       setCorrectAnswers(correctAnswers+1);
     }
     if(quizData.length === questionIndex+1){
-      setScreen(3)
+      setScreen(3);
+      setScorePercentage((correctAnswers/quizData.length) * 100);
+      if(scorePercentage >= 90){
+        setScoreGrade('A+')
+      }else if(scorePercentage >= 80){
+        setScoreGrade('A')
+      }else if(scorePercentage >= 70){
+        setScoreGrade('B')
+      }else if(scorePercentage >= 60){
+        setScoreGrade('C')
+      }else if(scorePercentage >= 50){
+        setScoreGrade('D')
+      }else{
+        setScoreGrade('F')
+      }
     }else{
       setQuestionIndex(questionIndex+1)
     }
@@ -122,7 +139,8 @@ function App() {
           <div className='container pt-5'>
             <div className='row'>
               <div className='offset-md-3 col-md-6 mb-4'>
-                <h2 className='text-white text-center'> Dear {userName}, You scored {correctAnswers} out of {quizData.length}</h2>
+                <h2 className='text-white text-center'> Dear {userName}, You scored {correctAnswers} out of {quizData.length}.</h2>
+                <p className='text-white text-center'>Your percentage is {scorePercentage}% & your grade is {scoreGrade}</p> 
                 <div className='mb-2 p-2 text-center'>
                   <button type="button" className='btn btn-success' onClick={()=>{setScreen(2);setQuestionIndex(0);setCorrectAnswers(0)}}>Restart <i className="fa-solid fa-repeat"></i></button>
                 </div>
